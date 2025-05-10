56a4d2af50f952653912d7bbc68cb702ad76ebb1
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2025-05-10 16:43:27 -0600

    vercel fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 import About from "@/components/About";
 import AudioBtn from "@/components/AudioBtn";
-import EmotionDetector from "@/components/EmotionDetector";
-import clsx from "clsx";
-import gsap from "gsap";
-import { useEffect, useRef, useState } from "react";
-import { TiLocationArrow } from "react-icons/ti";
 
 export default function Home() {
 	return (
